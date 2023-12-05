7c14986887ee922b809b70cc4e0a089870f55c11
Ilaydaan <152871922+Ilaydaan@users.noreply.github.com>
2023-12-05 12:58:41 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,11 @@
+SELECT * FROM country
+WHERE country LIKE 'A%a'
+
+SELECT * FROM country
+WHERE country LIKE '_____%n'
+
+SELECT * FROM film
+WHERE title ILIKE '%t%t%t%t%'
+
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length >90 AND rental_rate = 2.99
